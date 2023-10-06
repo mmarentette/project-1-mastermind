0d783a9e92a4c66d9466abead6a9bab5ea761be4
mmarentette <melrosemarentette@gmail.com>
2023-10-06 15:22:18 -0400

    Complete project setup

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// See wireframe here:
+// https://miro.com/app/board/uXjVNdHUIDI=/?share_link_id=168631429658
+console.log('app.js is loading!')
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
